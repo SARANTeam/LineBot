16ca1eef2588c76889e5cf97011b5c9abe47c061
SARANTeam <sunate.krasasung@gmail.com>
2016-11-03 16:36:48 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'GmrXlItq1vUkpCpqWQQL3PCTwsM8J2Jj9HE2SiVzsbIZS/vGjMX/OfsaZSNHe0435/8fVlKFcijutSSd0LJmUokkheuxzmIn3k/Sc36kgMsUoelIjVHO13WzQtX+GNFN8Yu7l9JVGiIjfXI2xWjRDAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
