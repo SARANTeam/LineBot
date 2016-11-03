427a00af4498d78d95789f03220b496e39cc64b7
SARANTeam <sunate.krasasung@gmail.com>
2016-11-03 16:48:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'บ้าหรือเปล่า อะ ๆ ยังอีก'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
